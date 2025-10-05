25968ab043ac4bc0b90223e1a547b09ba6d83e82
Anushri <anushritcs@gmail.com>
2025-10-05 16:02:44 +0000

    I cannot see the header

--- src/app/artisan/layout.tsx
+++ src/app/artisan/layout.tsx
@@ -22,18 +22,18 @@ export default function ArtisanLayout({
   return (
     <div className="flex h-full flex-col md:flex-row">
       <ArtisanSidebar />
-      <div className="flex flex-1 flex-col">
-        <main className="flex-1 overflow-y-auto bg-secondary/30">
+      <main className="flex-1 flex flex-col bg-secondary/30 overflow-hidden">
+        <div className="flex-1 overflow-y-auto">
           {children}
-          {pathname !== '/artisan/add-product' && (
+        </div>
+        {pathname !== '/artisan/add-product' && (
             <Link href="/artisan/add-product" className="fixed bottom-8 right-8 z-30">
               <Button size="icon" className="rounded-full h-14 w-14 bg-primary hover:bg-primary/90 shadow-lg">
                 <Plus className="h-6 w-6" />
               </Button>
             </Link>
           )}
-        </main>
-      </div>
+      </main>
     </div>
   );
 }
