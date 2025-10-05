27d132ce9dd69e0d1b6f6420a7c513c24b3ba4be
Anushri <anushritcs@gmail.com>
2025-10-05 15:39:55 +0000

    I have google cloud premium, how could you possible reach a quota limit

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,9 +1,10 @@
+
 'use server';
 import { config } from 'dotenv';
+config({ path: `.env.local` });
 config();
 
 import '@/ai/flows/community-trend-insights.ts';
 import '@/ai/flows/buyer-ai-designed-products.ts';
 import '@/ai/flows/translate-text.ts';
 import '@/ai/flows/generate-product-details.ts';
-
