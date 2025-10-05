da462196bf39afe88b461ca635ba3e897b956961
Anushri <anushritcs@gmail.com>
2025-10-05 10:26:05 +0000

    there is a clash in the screen now, fix the issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased flex items-center justify-center bg-zinc-200 dark:bg-zinc-800" suppressHydrationWarning>
         <LanguageProvider>
-          <div className={cn("relative w-full max-w-[420px] bg-background shadow-2xl overflow-hidden", isClient ? "h-screen" : "h-full")}>
+          <div className="relative w-full max-w-[420px] h-screen bg-background shadow-2xl overflow-hidden">
             <div className="h-full w-full overflow-y-auto">
               {children}
             </div>
