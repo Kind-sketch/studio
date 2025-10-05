ec1a1ea4bb719f65af630c6497cca4c9b0959655
Anushri <anushritcs@gmail.com>
2025-10-05 07:56:13 +0000

    Use this as splash screen and donot display any content:https://image2ur

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
-import { Logo } from './icons';
 
 export default function SplashScreen() {
   const splashImage = PlaceHolderImages.find((p) => p.id === 'splash-screen');
@@ -17,16 +16,6 @@ export default function SplashScreen() {
           priority
         />
       )}
-      <div className="absolute inset-0 bg-black/50" />
-      <div className="relative z-10 flex flex-col items-center text-center text-white">
-        <Logo className="h-24 w-24 text-primary-foreground" />
-        <h1 className="mt-4 font-headline text-5xl font-bold">
-          Artistry Havens
-        </h1>
-        <p className="mt-2 text-lg text-primary-foreground/80">
-          Where creativity finds a home.
-        </p>
-      </div>
     </div>
   );
 }
