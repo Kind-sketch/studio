96a8e64af5bc3fb53a41a1d70f99b22862693736
Anushri <anushritcs@gmail.com>
2025-10-10 03:39:17 +0000

    fix the front end display, make sure that it is visible in both mobile a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,10 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning>
+      <body className={cn("font-body antialiased", isClient && 'bg-zinc-200 dark:bg-zinc-800')} suppressHydrationWarning>
         <LanguageProvider>
           <TranslationProvider>
-            <div className="flex items-center justify-center min-h-screen w-full bg-zinc-200 dark:bg-zinc-800">
+            <div className="flex items-center justify-center min-h-screen w-full">
                 <div className="relative w-full max-w-[390px] h-screen max-h-[844px] bg-background shadow-2xl overflow-y-auto no-scrollbar">
                     {children}
                     {isClient && <Toaster />}
